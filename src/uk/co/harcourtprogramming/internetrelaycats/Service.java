15ee442f2138d29c127c1f6adb08f1535585859a
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-12-29 21:01:59 +0000

    Documented the Service Class

--- src/uk/co/harcourtprogramming/internetrelaycats/Service.java
+++ src/uk/co/harcourtprogramming/internetrelaycats/Service.java
@@ -8,18 +8,44 @@ import java.util.logging.Logger;
 import java.util.logging.LogRecord;
 import java.util.logging.Level;
 
+/**
+ * <p>The base class for Services for {@link RelayCat}</p>
+ * <p>Services form the main part of the user logic for a chat bot; they
+ * can send and receive messages, etc.</p>
+ */
 public abstract class Service
 {
+	/**
+	 * Stores the previously assigned
+	 */
 	private static int lastId = 0;
 
+	/**
+	 * @return The next sequential id for a service
+	 * @throws Error thrown if every integer has been assigned to a service
+	 */
 	private static synchronized int id()
 	{
-		return ++lastId;
+		++lastId;
+		if (lastId == 0) throw new Error("Out of available service ids");
+		return lastId;
 	}
 
+	/**
+	 * Assign the id to this service
+	 */
 	private final int id = id();
+	/**
+	 * Logger for this service
+	 */
+	@SuppressWarnings("NonConstantLogger") // Logger for *each* service
 	private final Logger log = Logger.getLogger("InternetRelayCats.Service." + id);
 
+	/**
+	 * <p>Create the general implementation of a Service</p>
+	 * <p>This initialises this services {@link #getId() id}, and the attached
+	 * logger</p>
+	 */
 	public Service()
 	{
 		final Handler h = new ConsoleHandler();
@@ -39,36 +65,97 @@ public abstract class Service
 		log.setUseParentHandlers(false);
 	}
 
+	/**
+	 * <p>Log a message, with associated Throwable information.</p>
+	 * <p>If the logger is currently enabled for the given message level
+	 * then the given arguments are stored in a LogRecord which is forwarded
+	 * to all registered output handlers.</p>
+	 * <p>Note that the thrown argument is stored in the LogRecord thrown
+	 * property, rather than the LogRecord parameters property.
+	 * Thus is it processed specially by output Formatters and is not treated
+	 * as a formatting parameter to the LogRecord message property.</p>
+	 * @param lvl One of the message level identifiers, e.g. SEVERE
+	 * @param msg The string message (or a key in the message catalog)
+	 * @param ex Throwable associated with log message.
+	 * @see Logger#log(java.util.logging.Level, java.lang.String, java.lang.Throwable)
+	 */
 	public void log(Level lvl, String msg, Throwable ex)
 	{
 		log.log(lvl, msg, ex);
 	}
 
+	/**
+	 * <p>Log a message, with an array of object arguments.</p>
+	 * <p>If the logger is currently enabled for the given message level then
+	 * a corresponding LogRecord is created and forwarded to all the registered
+	 * output Handler objects.</p>
+	 * @param lvl One of the message level identifiers, e.g. SEVERE
+	 * @param msg The string message (or a key in the message catalog)
+	 */
 	public void log(Level lvl, String msg)
 	{
 		log.log(lvl, msg);
 	}
 
+	/**
+	 * <p>Log a message, with an array of object arguments.</p>
+	 * <p>If the logger is currently enabled for the given message level then
+	 * a corresponding LogRecord is created and forwarded to all the registered
+	 * output Handler objects.</p>
+	 * @param lvl One of the message level identifiers, e.g. SEVERE
+	 * @param msg The string message (or a key in the message catalog)
+	 * @param params array of parameters to the message
+	 */
 	public void log(Level lvl, String msg, Object[] params)
 	{
 		log.log(lvl, msg, params);
 	}
 
+	/**
+	 * <p>Log a message, with associated Throwable information.</p>
+	 * <p>If the logger is currently enabled for the given message level
+	 * then the given arguments are stored in a LogRecord which is forwarded
+	 * to all registered output handlers.</p>
+	 * <p>Note that the thrown argument is stored in the LogRecord thrown
+	 * property, rather than the LogRecord parameters property.
+	 * Thus is it processed specially by output Formatters and is not treated
+	 * as a formatting parameter to the LogRecord message property.</p>
+	 * @param lvl One of the message level identifiers, e.g. SEVERE
+	 * @param ex Throwable associated with log message.
+	 * @see Logger#log(java.util.logging.Level, java.lang.String, java.lang.Throwable)
+	 */
 	public void log(Level lvl, Throwable ex)
 	{
 		log.log(lvl, null, ex);
 	}
 
+	/**
+	 * <p>This Service's service id</p>
+	 * <p>The service id is a unique, consecutively assigned integer used to
+	 * identify the service</p>
+	 * @return The service id
+	 */
 	protected final int getId()
 	{
 		return id;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
 	public String toString()
 	{
 		return this.getClass().getSimpleName() + '@' + this.getId();
 	}
 
+	/**
+	 * <p>Code that is to be run when the {@link RelayCat} instance is closed,
+	 * or this service is otherwise being removed from the instance.</p>
+	 * <p>This function should not be called by user code; to have external code
+	 * remove a service, use {@link IRelayCat#unregister(Service)
+	 * IRelayCat.unregister}</p>
+	 */
 	public abstract void shutdown();
 }
