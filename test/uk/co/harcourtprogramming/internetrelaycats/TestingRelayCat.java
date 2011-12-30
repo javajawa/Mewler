be27677e2867d5ece0170a6d72c597a75f66f17d
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-12-30 11:35:12 +0000

    Started creating proper unit tests
    The class TestingRelayCat emulates an irc connection without
    connecting to a remote server (although, a PircBot instance is initialised).
    It allows the tests to emulate being remote users, sending messages
    both directly and via channels to RelayCat.
    The relpies are stored in a queue, which the tests can examine for
    correctness.
    
    This has it's one set of unit tests, in TestingRelayCatTest, which
    also demonstrates some of the usage.

---
+++ test/uk/co/harcourtprogramming/internetrelaycats/TestingRelayCat.java
@@ -0,0 +1,141 @@
+package uk.co.harcourtprogramming.internetrelaycats;
+
+import java.util.LinkedList;
+import java.util.Queue;
+/**
+ *
+ *
+ */
+public class TestingRelayCat extends BasicRelayCat
+{
+	/**
+	 *
+	 */
+	private Queue<Message> messageQueue = new LinkedList<Message>();
+
+	/**
+	 *
+	 */
+	private static class TCatBot extends CatBot
+	{
+
+		/**
+		 *
+		 * @param name
+		 */
+		private TCatBot(String name)
+		{
+			super(name);
+		}
+
+		@Override
+		public String getNick()
+		{
+			return NAME;
+		}
+	}
+
+	/**
+	 *
+	 */
+	@SuppressWarnings("PublicInnerClass")
+	public static class RelayService extends Service implements MessageService
+	{
+
+		/**
+		 *
+		 */
+		public RelayService()
+		{
+			super();
+		}
+
+		@Override
+		public void shutdown()
+		{
+			// Nothing to see here. Move along, citizen.
+		}
+
+		@Override
+		public void handle(Message m)
+		{
+			if (m.isAction())
+			{
+				m.act(m.getSender(), m.getMessage());
+				if (m.getChannel() != null)
+					m.act(m.getChannel(), m.getMessage());
+			}
+			else
+			{
+				m.message(m.getSender(), m.getMessage());
+				if (m.getChannel() != null)
+					m.message(m.getChannel(), m.getMessage());
+			}
+		}
+
+	}
+
+	/**
+	 *
+	 */
+	public final static String NAME = "testBost";
+
+	/**
+	 *
+	 */
+	@SuppressWarnings("LeakingThisInConstructor")
+	public TestingRelayCat()
+	{
+		super(new TCatBot(NAME), "", null);
+		bot.setInst(this);
+	}
+
+	@Override
+	public synchronized void run()
+	{
+		throw new UnsupportedOperationException("TestingRelayCat is not for running; it is merely an input ouput queue");
+	}
+
+	@Override
+	public void message(String target, String message)
+	{
+		messageQueue.offer(new Message(message, "name", target, false, this));
+	}
+
+	@Override
+	public void act(String target, String action)
+	{
+		messageQueue.offer(new Message(action, "name", target, true, this));
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public Message getOutput()
+	{
+		return messageQueue.poll();
+	}
+
+	/**
+	 *
+	 * @param sender
+	 * @param channel
+	 * @param line
+	 */
+	public void inputMessage(String sender, String channel, String line)
+	{
+		bot.onMessage(channel, sender, null, null, line);
+	}
+
+	/**
+	 *
+	 * @param sender
+	 * @param channel
+	 * @param line
+	 */
+	public void inputAction(String sender, String channel, String line)
+	{
+		bot.onAction(sender, null, null, (channel==null ? NAME : channel), line);
+	}
+}
