ba523587eda6053eee4cb744d9ba8ecec2b3c355
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-01-01 15:17:49 +0000

    BasicRelayCatTest - some simple tests for BasicRelayCat

---
+++ test/uk/co/harcourtprogramming/internetrelaycats/BasicRelayCatTest.java
@@ -0,0 +1,59 @@
+package uk.co.harcourtprogramming.internetrelaycats;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BasicRelayCatTest
+{
+	/**
+	 * Blank public test constructor
+	 */
+	public BasicRelayCatTest()
+	{
+		// Nothing to see here. Move along, citizen!
+	}
+
+	/**
+	 * Nickname to use when testing
+	 */
+	private final static String NICK = "InternetRelayCats-test";
+	/**
+	 * Host to connect to when testing
+	 */
+	private final static String HOST = "irc.esper.net";
+
+	/**
+	 * Test of shutdown method, of class BasicRelayCat.
+	 *
+	 * @throws Throwable
+	 */
+	@Test(expected=ShutdownWasCalledException.class)
+	public void testShutdown() throws Throwable
+	{
+		BasicRelayCat instance = new BasicRelayCat(NICK, "localhost", null);
+		instance.addService(new ShutdownTestSerivce());
+
+		new Thread(instance).start();
+
+		try
+		{
+			instance.shutdown();
+		}
+		catch (RuntimeException ex)
+		{
+			throw ex.getCause();
+		}
+	}
+
+	/**
+	 * Test of getNick method, of class BasicRelayCat.
+	 */
+	@Test
+	public void testGetNick()
+	{
+		BasicRelayCat instance = new BasicRelayCat(NICK, "localhost", null);
+		// Until a connection is made, the Bot will return PircBot.
+		// This test only checks correct forwarding
+		assertEquals("PircBot", instance.getNick());
+	}
+}
