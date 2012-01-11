05eaa816232827eaf4708de66f7e18d73dbd8f07
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-01-11 14:37:55 +0000

    Added the beggining of tests for Mewler
    This only implemented test checks for the correct connection procedure
    for a given user, password, and name

---
+++ test/uk/co/harcourtprogramming/mewler/MewlerTest.java
@@ -0,0 +1,93 @@
+package uk.co.harcourtprogramming.mewler;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MewlerTest
+{
+	public MewlerTest()
+	{
+	}
+
+	/**
+	 * Test of connect method, of class Mewler.
+	 */
+	@Test(timeout=8000)
+	public void testConnect() throws Exception
+	{
+		final String nick = "bob";
+		final String password = "pass";
+		final String realName = "Bobby";
+
+		final LocalBufferedSocket inputSocket = new LocalBufferedSocket();
+		final LocalBufferedSocket outputSocket = new LocalBufferedSocket();
+
+		final LocalBufferedSocket.LineReader fromClient = outputSocket.in;
+		final BufferedWriter toClient = new BufferedWriter(new OutputStreamWriter(inputSocket.out));
+
+		final Mewler instance = new Mewler(inputSocket.in, outputSocket.out, null);
+		final Thread main = Thread.currentThread();
+
+		final Thread testHolder = new Thread() {
+			private Exception e;
+			@Override
+			public void run()
+			{
+				try
+				{
+					instance.connect(nick, password, realName);
+				}
+				catch (IOException ex)
+				{
+					e = ex;
+				}
+				finally
+				{
+					synchronized (main)
+					{
+						main.notifyAll();
+					}
+				}
+			}
+
+			@Override
+			public ClassLoader getContextClassLoader()
+			{
+				if (e != null) throw new Error(e);
+				return super.getContextClassLoader();
+			}
+		};
+
+		toClient.append(":localhost 375 :- <server> Message of the day - \r\n");
+		toClient.flush();
+
+		synchronized (main)
+		{
+			testHolder.start();
+			main.wait();
+		}
+
+		testHolder.getContextClassLoader();
+		String line;
+
+		line = fromClient.readLine();
+		assertNotNull(line);
+		assertEquals(IrcCommands.createCommandString(IrcCommands.PASS, password), line + "\r\n");
+
+		line = fromClient.readLine();
+		assertNotNull(line);
+		assertEquals(IrcCommands.createCommandString(IrcCommands.NICK, nick), line + "\r\n");
+
+		line = fromClient.readLine();
+		assertNotNull(line);
+		assertEquals(IrcCommands.createCommandString(IrcCommands.USER, nick, 0, "*", realName), line + "\r\n");
+
+		assertFalse(testHolder.isAlive());
+
+		instance.dispose();
+	}
+
+}
