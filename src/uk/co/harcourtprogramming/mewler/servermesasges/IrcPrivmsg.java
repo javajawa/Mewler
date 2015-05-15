4d17b79ad9a34e1c715547643a7e01122d121aab
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2015-05-15 21:22:17 +0100

    Support for CTCP messages
     - Add a command field to IrcPrivmsg
     - Add Commands 'NONE' and 'INVALID'

--- src/uk/co/harcourtprogramming/mewler/servermesasges/IrcPrivmsg.java
+++ src/uk/co/harcourtprogramming/mewler/servermesasges/IrcPrivmsg.java
@@ -1,9 +1,31 @@
 package uk.co.harcourtprogramming.mewler.servermesasges;
+
 import uk.co.harcourtprogramming.mewler.MessageTokeniser;
+
 public class IrcPrivmsg extends AbstractIrcMessage
 {
+	/**
+	 * <p>Name of the User or Channel this message came from</p>
+	 *
+	 * @todo Refactor to be "origin" or similar to make it clearer this may be a nick
+	 */
 	public final String channel;
+	/**
+	 * <p>String content of the message</p>
+	 */
 	public final String message;
+	/**
+	 * <p>The Client-to-Client protocol 'verb', if this is a CTCP message,
+	 * NONE otherwise</p>
+	 * @link PrivmsgExtensionCommands#NONE
+	 */
+	public final PrivmsgExtensionCommands command;
+	/**
+	 * <p>If this message is a CTCP command with 'verb' "ACTION"</p>
+	 *
+	 * @link PrivmsgExtensionCommands#ACTION
+	 * @todo Deprecate this.
+	 */
 	public final boolean action;
 
 	public IrcPrivmsg(String origin, MessageTokeniser payload, String selfNick)
@@ -18,18 +40,29 @@ public class IrcPrivmsg extends AbstractIrcMessage
 		if (payload.startsWith("\u0001"))
 		{
 			payload.consume("\u0001");
-			switch (PrivmsgExtensionCommands.valueOf(payload.nextToken(' ')))
+
+			PrivmsgExtensionCommands commandTemp;
+			final String commandString = payload.nextToken(' ');
+
+			try
 			{
-				case ACTION:
-					action = true;
-					break;
-				default:
-					action = false;
+				commandTemp = PrivmsgExtensionCommands.valueOf(commandString);
 			}
+			catch (IllegalArgumentException ignore)
+			{
+				commandTemp = PrivmsgExtensionCommands.INVALID;
+			}
+			command = commandTemp;
+
+			// Set legacy flag for IRC 'Action'
+			action = (command == PrivmsgExtensionCommands.ACTION);
+			// Extract the message
+			// TODO: This might need unescaping?
 			message = payload.nextToken('\u0001');
 		}
 		else
 		{
+			command = PrivmsgExtensionCommands.NONE;
 			action = false;
 			message = payload.toString();
 		}
@@ -40,6 +73,5 @@ public class IrcPrivmsg extends AbstractIrcMessage
 	{
 		return origin + (channel != null ? " via " + channel : "") + " said " + message;
 	}
-
 
 }
--- src/uk/co/harcourtprogramming/mewler/servermesasges/PrivmsgExtensionCommands.java
+++ src/uk/co/harcourtprogramming/mewler/servermesasges/PrivmsgExtensionCommands.java
@@ -2,5 +2,17 @@ package uk.co.harcourtprogramming.mewler.servermesasges;
 
 public enum PrivmsgExtensionCommands
 {
+	/**
+	 * Message was not a CTCP Command
+	 */
+	NONE,
+	/**
+	 * Message was formatted as a CTCP command, but the command name was
+	 * not recognised
+	 */
+	INVALID,
+	/**
+	 * Message was an IRC action
+	 */
 	ACTION;
 }
